23c1b326431d7a5dfd76e97791ce460871ab2a3b
王澍(Andrew) <andrew7810262000@gmail.com>
2020-11-02 10:10:18 +0800

    add material-icons

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,7 @@
 <template>
     <div id="app">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+              rel="stylesheet">
         <router-view/>
     </div>
 </template>

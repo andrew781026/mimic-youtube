c443e3f197c7206a3ad6c4c12a70f95325ad1ea2
王澍(Andrew) <andrew7810262000@gmail.com>
2020-11-03 11:12:57 +0800

    add test-case in test folder

--- front/src/utils/youtubeUtils.js
+++ front/src/utils/youtubeUtils.js
@@ -1,35 +1,68 @@
-/*
-
-GET https://youtube.googleapis.com/youtube/v3/videos?key=[YOUR_API_KEY] HTTP/1.1
-
-Authorization: Bearer [YOUR_ACCESS_TOKEN]
-Accept: application/json
-
-*/
-
-// 參考資料 : https://developers.google.com/youtube/v3/quickstart/nodejs
-// https://medium.com/%E5%B0%8F%E9%83%AD-%E0%B9%80%E0%B8%88%E0%B8%99/%E8%8F%9C%E9%B3%A5%E5%B7%A5%E7%A8%8B%E5%B8%AB-youtube-data-api-%E8%BC%89%E5%85%A5%E6%92%AD%E6%94%BE%E6%B8%85%E5%96%AE%E4%B8%A6%E5%88%87%E6%8F%9B%E6%AD%8C%E6%9B%B2-356d8e454ca3
+/**
+ * 參考資料 : https://developers.google.com/youtube/v3/quickstart/nodejs
+ * https://medium.com/%E5%B0%8F%E9%83%AD-%E0%B9%80%E0%B8%88%E0%B8%99/%E8%8F%9C%E9%B3%A5%E5%B7%A5%E7%A8%8B%E5%B8%AB-youtube-data-api-%E8%BC%89%E5%85%A5%E6%92%AD%E6%94%BE%E6%B8%85%E5%96%AE%E4%B8%A6%E5%88%87%E6%8F%9B%E6%AD%8C%E6%9B%B2-356d8e454ca3
+ * https://developers.google.com/youtube/v3/docs/videos/list
+ * */
 const axios = require('axios');
 const API_KEY = 'AIzaSyA1HsDdjJDtuw2L3zVMA_9lWdrqn5K6_J4';
 
-// a very simple example of searching for youtube videos
-// 私人的撥放清單 , 需要用 OAuth 登入後才能取得
-async function runSample() {
+/**
+ * part 中可填寫的參數
+ *   🔸 contentDetails - 影片詳細資訊 ( 影片長度 ...等 )
+ *   🔸 fileDetails   -
+ *   🔸 id   - 影片編號
+ *   🔸 liveStreamingDetails
+ *   🔸 localizations
+ *   🔸 player
+ *   🔸 processingDetails
+ *   🔸 recordingDetails
+ *   🔸 snippet - channelId, title, description, tags, and categoryId
+ *   🔸 status
+ *   🔸 suggestions
+ *   🔸 topicDetails
+ * */
 
-    // create playlist
+/**
+ * 查詢 Youtube 上的影片
+ * @param {string} id - Youtube 影片編號
+ * @param {string} part - 必填，把需要的回傳資訊列表出來
+ * @param {string} chart - 圖表類型，目前只有參數 mostPopular 可用，用於回傳指定 "地區" 與 "類型" 最受歡迎的影片
+ * @param {number} maxResults - 回傳筆數上限，預設為五筆資料，可以設定 1 ~ 50
+ * @param {string} myRating - OAuth 後才能用的參數，參數： like / dislike 列表出個人喜歡與不喜歡的影片
+ * @param {string=} pageToken - 分頁時查詢上一頁 & 下一頁所用的 token
+ * @param {string=} regionCode - 區域代碼
+ * @param {string=} videoCategoryId - 影片分類編號
+ * @param {string} q - 查詢文字
+ */
+const searchVideo = async ({
+                               id,
+                               part,
+                               chart,
+                               maxResults,
+                               q,
+                               myRating,
+                               pageToken,
+                               regionCode,
+                               videoCategoryId
+                           }) => {
+
+    // list the video you want to search
     const res = await axios.get('https://www.googleapis.com/youtube/v3/videos',
         {
             params: {
-                id:'7lCDEYXw3mM',
-                // chart:'mostPopular',
-                part: 'id,snippet',// 必填，把需要的資訊列出來
-                // q: '韓國瑜',// 查詢文字
-                // maxResults: 50,// 預設為五筆資料，可以設定1~50
+                id,
+                chart,
+                part,
+                q,
+                maxResults,
+                myRating,
                 key: API_KEY, // 使用 API 只能取得公開的播放清單
             }
         });
 
-    console.log(res.data);
-}
+    return res.data;
+};
 
-runSample().catch(err => console.error(err.response.data));
+const YoutubeUtils = {searchVideo};
+
+export default YoutubeUtils;
---
+++ test/__tests__/unit/YoutubeUtils-test.js
@@ -0,0 +1,41 @@
+import YoutubeUtils from '../../../front/src/utils/youtubeUtils';
+
+// TODO 需要讓 Jest 可以使用 import . export
+describe('測試 ', () => {
+
+    beforeEach(function () {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000;
+    });
+
+    it('查詢影片:編號 coZbOM6E47I', async () => {
+
+        // 下方區塊需要放入單元測試中
+        const data = await YoutubeUtils.searchVideo({part: 'snippet,contentDetails', id: 'coZbOM6E47I'})
+
+        const snippets = data.items.map(item => {
+
+            const snippet = item.snippet;
+            const contentDetails = item.contentDetails;
+
+            return {
+                contentDetails: contentDetails.duration,   // PT2H37M38S
+                publishedAt: snippet.publishedAt,
+                title: snippet.title,
+                description: snippet.description,
+                thumbnails: snippet.thumbnails.standard,
+            }
+        })
+
+        const item = data.items[0];
+
+        expect(item.snippet.title).toBeDefined();               // 標題
+        expect(item.snippet.description).toBeDefined();         // 描述
+        expect(item.snippet.publishedAt).toBeDefined();         // 發布時間
+        expect(item.snippet.thumbnails.standard).toBeDefined(); // 圖片
+        expect(item.contentDetails.duration).toBeDefined();     // 影片長度
+
+        // 如果要了解回傳的資料結構可參考 youtube-video-sample-info.json
+        // console.log(JSON.stringify(data));
+    })
+
+})

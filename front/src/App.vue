d81aef704278dbad3cecc99a14759c223e98f10d
王澍(Andrew) <andrew7810262000@gmail.com>
2020-11-09 17:20:31 +0800

    改使用 font-awesome 5.13

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
         <router-view/>
     </div>
 </template>
